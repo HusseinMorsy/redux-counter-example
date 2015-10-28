edf76fbc7735cf969348f5d8a45c88130521fbca
Hussein Morsy <hussein@morsy.de>
2015-10-28 21:09:36 +0100

    remove reducer example

--- index.js
+++ index.js
@@ -27,12 +27,3 @@ store.dispatch({ type: 'INCREMENT' });
 store.dispatch({ type: 'DECREMENT' });
 store.dispatch({ type: 'INCREMENT_BY', value: 10 });
 store.dispatch({ type: 'RESET' });
-
-
-
-// simple reducer example
-const reducer = function(acc, item) {
-  return acc + item;
-}
-
-console.log([1,2,3].reduce(reducer, 0));
